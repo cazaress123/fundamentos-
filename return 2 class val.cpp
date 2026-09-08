73e3502aeb1216374e0dc67d90d712bbbc3597f6
cazaress123 <26040024@itdurango.edu.mx>
2026-09-08 13:19:19 -0600

    Add files via upload

---
+++ return 2 class val.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+float CalcularArea(base,altura) {
+	float area;
+	cout<<"El resultado seria: ";
+area=base*altura/2;
+return area;
+
+} 
+
+int main() {
+cout<<"Calculadora ";
+float base,altura;
+float area;
+cout<<"teclea base ";
+cin>>base;
+cout<<"teclea altura ";
+cin>>altura;
+// calcular area
+cout<<"El resultado seria: ";
+cout<<"Area= "<<;
+cout<<"El area del triangulo es" CalcularArea(base,altura)
+	return 1;
+}
